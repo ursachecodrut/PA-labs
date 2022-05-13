31f34fdb19661f7964374766fbd0ca55fbb333b3
Codrut Ursache <codrutursache@pop-os.localdomain>
2022-05-13 17:55:11 +0300

    bfs

--- skel/lab07/java/task-1/src/Main.java
+++ skel/lab07/java/task-1/src/Main.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Queue;
 
 public class Main {
     static class Task {
@@ -13,7 +17,9 @@ public class Main {
         public static final String OUTPUT_FILE = "out";
 
         // numarul maxim de noduri
-        public static final int NMAX = (int)1e5 + 5; // 10^5 + 5 = 100.005
+        public static final int NMAX = (int) 1e5 + 5; // 10^5 + 5 = 100.005
+
+        public static final int INF = (int) 1e9;
 
         // n = numar de noduri, m = numar de muchii/arce
         int n, m;
@@ -34,7 +40,7 @@ public class Main {
         private void readInput() {
             try {
                 Scanner sc = new Scanner(new BufferedReader(new FileReader(
-                                INPUT_FILE)));
+                        INPUT_FILE)));
                 n = sc.nextInt();
                 m = sc.nextInt();
                 source = sc.nextInt();
@@ -60,7 +66,7 @@ public class Main {
         private void writeOutput(int[] d) {
             try {
                 PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(
-                                OUTPUT_FILE)));
+                        OUTPUT_FILE)));
                 for (int node = 1; node <= n; node++) {
                     pw.printf("%d%c", d[node], node == n ? '\n' : ' ');
                 }
@@ -70,17 +76,51 @@ public class Main {
             }
         }
 
+        int[] BFS(int source, ArrayList<Integer> adj[]) {
+            int d[] = new int[n + 1];
+            int p[] = new int[n + 1];
+            // fill d with INF values
+            Arrays.fill(d, INF);
+            Arrays.fill(p, -1);
+
+            // init queue
+            Queue<Integer> queue = new LinkedList<Integer>();
+
+            d[source] = 0;
+            p[source] = -1;
+            queue.add(source);
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                for (Integer neigh : adj[node]) {
+                    if (d[node] + 1 < d[neigh]) {
+                        d[neigh] = d[node] + 1;
+                        p[neigh] = node;
+                        queue.add(neigh);
+                    }
+                }
+            }
+
+            for (int node = 1; node <= n; node++) {
+                if (d[node] == INF) {
+                    d[node] = -1;
+                }
+            }
+
+            return d;
+        }
+
         private int[] getResult() {
             // TODO: Faceti un BFS care sa construiasca in d valorile:
-            // * d[node] = numarul minim de muchii de parcurs de la nodul `source` la nodul `node`
-            //      * d[source] = 0
-            //      * d[node]   = -1, daca nu exista drum de la source la node
+            // * d[node] = numarul minim de muchii de parcurs de la nodul `source` la nodul
+            // `node`
+            // * d[source] = 0
+            // * d[node] = -1, daca nu exista drum de la source la node
             // *******
             // ATENTIE: nodurile sunt indexate de la 1 la n.
             // *******
 
-            int d[] = new int[n + 1];
-            return d;
+            return BFS(source, adj);
         }
     }
 
